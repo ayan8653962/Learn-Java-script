b13b3166dfbb428087f835a6dc1b8945f826541a
ayan8653962 <ayan8653962@gmail.com>
2025-08-28 15:44:13 +0500

    first commist

---
+++ app.js
@@ -0,0 +1,29 @@
+// alert("Wellcome To Rental Website")
+//var eq1=prompt("What is you name")  
+//var eq2=prompt("What is your father name")
+//var res=(eq1+""+eq2)
+//alert("Thanks for Details SHare")
+//document.writeln(res)
+//var name1 =prompt('what is you name')
+//alert( 'Hello' + name1)
+//var name1 = +prompt("Enter Number")
+//var name2 = +prompt("Enter Numbar")
+//var name3 = prompt("Enter Number")
+//var name3 = prompt("Enter Number")
+//document.writeln(name1 + name2 - name3)
+//var zeeshan = prompt("Enter your city name")
+//alert(" Do You live in" + zeeshan)
+//var name1 = +prompt("Enter number")
+//var name2 = +prompt("Enter number")
+//var name3 = +prompt("Enter number")
+//alert(name1 + name2 + name3)
+   
+
+
+
+
+
+
+
+
+

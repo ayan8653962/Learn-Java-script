755d910fbc0c7f1b8e6f306962162f133032dc8b
ayan8653962 <ayan8653962@gmail.com>
2025-08-29 02:19:43 +0500

    zeeshan practice

--- app.js
+++ app.js
@@ -17,7 +17,79 @@
 //var name2 = +prompt("Enter number")
 //var name3 = +prompt("Enter number")
 //alert(name1 + name2 + name3)
-   
+//if statment   
+//if(7<5){
+   // document.writeln("this is correct")
+//}
+//else{
+  //   document.writeln("this is wrong")
+//}
+//var name1 =prompt ("Enter Name")
+//var name2 =prompt ("Enter Passowd")
+
+
+//if(name1 == name2){
+  //  alert("login successfully")
+//}
+//else{
+    //document.writeln("Invalid Username")
+//}
+//= assigning oprater 
+// == sirf value match karta ha 
+// === value aur data type dono match karaga 
+// && left and righ dono ka right hona zaroori ha 
+// || koi bhi aik right kardo ma aga la gaonga 
+// < less then 
+// >  greater then
+// <= less then equal to 
+// >= greater then equal to 
+// != not eual to  
+
+//var Name =prompt ("Enter Name")
+// document.writeln(Name)
+//var marks1 = +prompt ("Enter English Subject Marks")
+//var marks2 = +prompt ("Enter Urdu Subject Marks")
+//var marks3 = +prompt ("Enter Islamiyat Subject Marks")
+//var marks4 = +prompt ("Enter Boilogy Subject Marks")
+//var marks5 = +prompt ("Enter Chemistry Subject Marks")
+//var total =(marks1 + marks2 + marks3 + marks4 + marks5 )
+//var persan =(total / 500 * 100 + "%")  
+// document.writeln(persan)
+
+var persan = prompt('Enter Persantage')
+if(persan >= 90 && persan < 101){
+  document.writeln("A+" + "" + "Grade")
+}
+else if(persan >= 80 && persan < 90){
+  document.writeln("A1" + "" + "Grade")
+
+}
+else if(persan >= 70 && persan < 80){
+  document.writeln("A"  + "" + "Grade")
+
+}else if(persan >= 60 && persan < 70){
+  document.writeln("B"  + "" + "Grade")
+
+}else if(persan >= 50 && persan < 60){
+  document.writeln("C"  + "" + "Grade")
+
+}else if(persan >= 40 && persan < 50){
+  document.writeln("D"  + "" + "Grade")
+
+}
+else if(persan >= 34 && persan < 40){
+  document.writeln("E"  + "" + "Grade")
+
+}
+else if(persan >= 1 && persan < 34){
+  document.writeln("fail Ho Ap ")
+}
+
+
+
+
+
+
 
 
 
